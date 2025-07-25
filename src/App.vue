75f47cf3a505eb5c38eb77f68fcbae57d7ff1fb2
AcepHp <acephandika27@gmail.com>
2025-07-25 15:09:42 +0700

    feat: section-1 with animation gsap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import HeaderSection from './components/Header.vue'
+  import Section1 from './components/Section-1.vue';
 </script>
 
 <template>
   <HeaderSection/>
+  <Section1/>
 </template>
---
+++ src/components/Section-1.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ref, onMounted, nextTick } from 'vue'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const vidElement = ref(null)
+
+onMounted(async () => {
+    await nextTick()
+    gsap.from(vidElement.value, {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        ease: 'power2.out',
+        scrollTrigger: {
+            trigger: vidElement.value,
+            start: 'top 80%',
+            end: 'bottom 20%',
+            toggleActions: 'play reverse play reverse',
+        },
+    })
+})
+</script>
+
+<template>
+    <section class="about-section">
+        <div class="content-wrapper">
+            <h2>We are</h2>
+            <div>
+                team of like-minded people, socially active companies, media personalities, volunteers who
+                help low-income shelters and foster carers to support homeless animals.
+            </div>
+            <h2>Our primary goal</h2>
+            <div>
+                is to provide animals with the most necessary things - food, medicine, and urgent needs for
+                the livelihood of shelters. We believe in our power and the power of conscious citizens who
+                care about the environment and their health, who understand the importance of helping each
+                other, especially our little friends. Growth begins with your care for your loved ones.
+                Animals are one of the closest creatures to humans who remain with us throughout our lives.
+                We all know the healing power of warmth, grace, and big loving eyes.
+            </div>
+            <blockquote>
+                "Animals need to have friends. Just like humans." <br />
+                - James Herriot, All Creatures Great and Small
+            </blockquote>
+            <div>
+                Who else if not we should support our younger brothers, especially in difficult times?
+                Let's be friends!
+            </div>
+            <div class="media-box" ref="vidElement">
+                <iframe loading="lazy" src="https://www.youtube.com/embed/5Wk1rp99B7o?start=12"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+</template>
